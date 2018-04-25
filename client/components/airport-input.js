2dbb3d2a3da03aaf7d0b7a5fac129f396c09361b
Sam Parl <samaparl@gmail.com>
2018-04-25 09:32:21 -0400

    Fix autocomplete componnet

--- client/components/airport-input.js
+++ client/components/airport-input.js
@@ -1,19 +1,53 @@
 const React = require('react');
 const styles = require('./airport-input.scss');
-import { AutocompleteDropdown } from './autocomplete-dropdown';
 
 export class AirportInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { curr: -1 };
+    this.ref = React.createRef();
+  }
+
+  handleKeyDown(event) {
+    if ([13, 38, 40].includes(event.keyCode)) { event.preventDefault(); }
+    const curr = this.state.curr;
+    const ref = this.ref.current;
+    switch (event.keyCode) {
+      case 40:
+        if (curr < this.props.options.length - 1) this.setState({ curr: curr + 1 });
+        break;
+      case 38:
+        if (curr > 0) this.setState({ curr: curr - 1 });
+        break;
+      case 13:
+        if (ref) {
+          const change = new Event('click', { bubbles: true });
+          ref.dispatchEvent(change);
+        }
+        break;
+    }
+  }
+
   render() {
     const regex = new RegExp(this.props.value, 'i');
     const options = this.props.options.filter(val => val.match(regex) && val !== this.props.value);
-    const select = this.props.value && options.length ?
-      <AutocompleteDropdown onChange={ this.props.onChange } options={ options } /> : null;
+    const dropdown = (
+      <ul className="autocomplete-dropdown" onClick={ this.props.onChange }>
+        {
+          this.props.options.map((option, index) =>
+          <option className={ this.state.curr === index ? 'current' : null }
+            ref={ this.state.curr === index ? this.ref : null }
+            key={ index }
+            value={ option }>{ option }</option>)
+        }
+      </ul>
+    );
 
     return (
-      <div className="airport-input">
+      <div className="airport-input" onKeyDown={ this.handleKeyDown.bind(this) }>
         <input value={ this.props.value } onChange={ this.props.onChange } />
         {
-          select
+          this.props.value && options.length ? dropdown : null
         }
       </div>
     );
--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const parser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const server = express();
 server.use(parser.json());
@@ -9,8 +11,22 @@ server.use(express.static(path.join(__dirname, '/')));
 
 const PORT = process.env.port || 4000;
 
+const AIRPORT_API = `https://aviation-edge.com/api/public/airportDatabase?key=${ process.env.AIRPORT_API_KEY }&&codeIso2Country=US`;
+const AIRPORT_DATA;
+
+const handleAirportData = (res) => {
+  if (AIRPORT_DATA) {
+    res.sendData(AIRPORT_DATA);
+  }
+  else {
+    request(AIRPORT_API).then(response => {
+      AIRPORT_DATA = response;
+      res.sendData(AIRPORT_DATA);
+    });
+  }
+}
+
 const serveIndex = (request, response) => {
-  console.log('Request received.');
   response.sendFile(path.resolve(__dirname, 'client/index.html'));
 };
 
